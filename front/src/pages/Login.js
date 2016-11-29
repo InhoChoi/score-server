c6043bdcadbcaaedd6c6cfd30a6f513aeec0f524
inhochoi <revaer@naver.com>
2016-11-29 17:19:43 +0900

    Front: Add Login Pages

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -4,18 +4,66 @@ import { login } from '../actions/auth'
 
 class Login extends React.Component {
   componentWillMount() {
+    this.state = {email: '', password: ''};
+    this.handlerLogin = this.handlerLogin.bind(this);
+    this.handlerChangeEmail = this.handlerChangeEmail.bind(this);
+    this.handlerChangePassword = this.handlerChangePassword.bind(this);
+  }
+
+  handlerChangeEmail(event){
+    this.setState({email: event.target.value});
+  }
+
+  handlerChangePassword(event){
+    this.setState({password: event.target.value});
+  }
+
+  handlerLogin(event){
+    const { login } = this.props;
+
+    event.preventDefault();
+    login(this.state.email, this.state.password);
+  }
+
+  _errorRender(){
+    const { login_error } = this.props;
+    if( login_error === false) return '';
+
+    return (
+      <p className="bg-danger text-center" style={{padding: '5px', 'margin-bottom': '0px'}}>로그인 실패하였습니다</p>
+    )
   }
   render() {
-    const { login } = this.props;
     return (
-      <div className="container">
-        <div className="col-md-12">
-          <a className="col-md-12 btn btn-default" onClick={() => login("2","3")}>Login</a>
+      <div>
+        <div className="text-center">
+          <h1> Score Server </h1>
+        </div>
+        <br/>
+        <div className="col-md-offset-4 col-md-4">
+          <div className="panel panel-default">
+            {this._errorRender()}
+            <div className="panel-body">
+              <form onSubmit={this.handlerLogin}>
+                <div className="form-group">
+                  <label htmlFor="email">이메일</label>
+                  <input type="email" className="form-control" onChange={this.handlerChangeEmail} placeholder="Email"/>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">암호</label>
+                  <input type="password" className="form-control" onChange={this.handlerChangePassword} placeholder="Password"/>
+                </div>
+                <button type="submit" className="btn btn-primary">로그인</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  login_error: state.auth.error
+});
 export default connect(mapStateToProps, {login})(Login);
--- front/src/reducers/auth.js
+++ front/src/reducers/auth.js
@@ -2,19 +2,20 @@ import { LOGIN_WAITING, LOGIN_SUCCESS, LOGIN_FAIL } from '../constants'
 
 const initialState = {
   fetching: false,
+  error: false,
   token: '',
   userid: ''
 };
 
 export default function auth(state = initialState, action) {
   if(action.type === LOGIN_WAITING) {
-    return { fetch: false, token: '', userid: ''}
+    return { fetch: false, error: false, token: '', userid: ''}
   }
   else if(action.type === LOGIN_SUCCESS) {
-    return { fetch: true, token: action.token, userid: action.userid}
+    return { fetch: true, error: false, token: action.token, userid: action.userid}
   }
   else if(action.type === LOGIN_FAIL) {
-    return { fetch: false, token: '', userid: ''}
+    return { fetch: false, error: true, token: '', userid: ''}
   }
   return state
 }
