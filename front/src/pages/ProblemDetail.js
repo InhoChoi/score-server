eb03c316d3ae24c63b696c881de3315e67e8eca5
inhochoi <revaer@naver.com>
2016-12-01 22:36:16 +0900

    Front: Add Problem Detail Pages

--- front/src/pages/ProblemDetail.js
+++ front/src/pages/ProblemDetail.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { browserHistory, Link } from 'react-router'
 
-function ProblemDetail() {
+function ProblemDetail({ params, problems }) {
+  const problem = problems.filter((problem) => problem.id === parseInt(params.id, 10));
+  if( problem.length === 0 ) {
+    browserHistory.push('/problem');
+    return <div></div>;
+  }
+
   return(
     <div>
-      <h1>ProblemDetail</h1>
+      <div className="panel panel-info">
+        <div className="panel-heading">
+          <h1 className="panel-title">{problem[0].title}</h1>
+        </div>
+        <div className="panel-body">
+          {problem[0].content}
+        </div>
+        <div className="panel-footer">
+          <Link className="btn btn-info text-left" to={"/problem/"+problem[0].id+"/submit"}>제출하기</Link>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default ProblemDetail;
+
+const mapStateToProps = (state) => ({
+  problems: state.problem.problems
+});
+export default connect(mapStateToProps)(ProblemDetail);
