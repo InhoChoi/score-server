5e0dea24d59a0c80ca62443fb3c75e3e0493bae0
inhochoi <revaer@naver.com>
2016-12-02 12:56:42 +0900

    Front: Implemnet ResultDetail pages

--- front/src/pages/ResultDetail.js
+++ front/src/pages/ResultDetail.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
 
-function ResultDetail() {
+function ResultDetail({ results, params }) {
+  const result = results.filter((result) => result.id === parseInt(params.id, 10));
+  if( result.length === 0 ) {
+    browserHistory.push('/result');
+    return <div></div>;
+  }
+
+  let status = '';
+  if( result[0].status === 'Correct'){
+    status = <p className="btn btn-success">정답</p>;
+  }else if( result[0].status === 'Wrong'){
+    status = <p className="btn btn-danger">오답</p>;
+  }else if( result[0].status === 'Complie Error'){
+    status = <p className="btn btn-danger">컴파일 에러</p>;
+  }else{
+    status = <p className="btn btn-warning">대기중</p>;
+  }
+
+
   return(
     <div>
-      <h1>ResultDetail</h1>
+        <div className="panel panel-info">
+          <div className="panel-heading">결과 상세정보</div>
+          <ul className="list-group">
+            <li className="list-group-item">{status}</li>
+          </ul>
+          <ul className="list-group">
+            <li className="list-group-item">제출 된코드</li>
+          </ul>
+          <div className="panel-body">
+            <div className="form-group">
+              <textarea className="form-control" rows="8" value={result[0].code} readOnly></textarea>
+            </div>
+
+          </div>
+          <ul className="list-group">
+            <li className="list-group-item">컴파일 에러 메시지</li>
+          </ul>
+          <div className="panel-body">
+            <textarea className="form-control" rows="8" value={result[0].output} readOnly></textarea>
+          </div>
+        </div>
     </div>
   )
 }
 
-export default ResultDetail;
+const mapStateToProps = (state) => ({
+  results: state.result.results
+});
+export default connect(mapStateToProps)(ResultDetail);
